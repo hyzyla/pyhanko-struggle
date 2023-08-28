6d96d5a691b7d1e9f4910606f365a3571c875ad4
Yevhenii Hyzyla <hyzyla@gmail.com>
2023-08-28 21:43:34 +0000

    Add stamp

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import logging
 
 from pyhanko.pdf_utils.incremental_writer import IncrementalPdfFileWriter
 from pyhanko.pdf_utils.reader import PdfFileReader
-from pyhanko.sign import signers
+from pyhanko.sign import signers, fields
 from pyhanko.sign.fields import SigSeedSubFilter
 from pyhanko.sign.validation import async_validate_pdf_signature
 from pyhanko.sign.validation.pdf_embedded import EmbeddedPdfSignature
 from pyhanko_certvalidator import ValidationContext
 from pyhanko_certvalidator.policy_decl import AcceptAllAlgorithms
+from pyhanko import stamp
 
 logging.basicConfig(
     # level=logging.DEBUG,
@@ -27,6 +28,14 @@ async def main():
 
     with open("input.pdf", "rb") as inf:
         w = IncrementalPdfFileWriter(inf)
+        fields.append_signature_field(
+            w,
+            sig_field_spec=fields.SigFieldSpec(
+                sig_field_name='MobileIdVnExample',
+                box=(200, 600, 400, 660),
+            ),
+        )
+    
         validation_context = ValidationContext(
             allow_fetching=False,
             trust_roots=[chain[-1]],
@@ -42,12 +51,21 @@ async def main():
             use_pades_lta=False,
         )
         with open("output.pdf", "wb") as outf:
-            await signers.async_sign_pdf(
-                w,
+            pdf_signer = signers.PdfSigner(
                 meta,
-                signer=signer,
+                signer,
+                timestamper=None,
+                new_field_spec=None,
+                stamp_style=stamp.StaticStampStyle.from_pdf_file('stamp.pdf', border_width=0),
+            )
+            await pdf_signer.async_sign_pdf(
+                w,
+                existing_fields_only=False,
+                bytes_reserved=None,
+                in_place=False,
                 output=outf,
             )
+     
 
         print("\n\n============ VALIDATION ============")
         with open("output.pdf", "rb") as outf:
